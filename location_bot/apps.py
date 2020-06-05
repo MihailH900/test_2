d49b14287cf2229a3c3c9b02b730eafa1672b9cf
Mihail <alxumik@gmail.com>
2020-06-05 15:08:00 +0300

    clear

--- location_bot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocationBotConfig(AppConfig):
-    name = 'location_bot'
--- location_bot/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class VK_sender(models.Model):
-        user_id = models.TextField(max_length = 100, blank = True)
-        meet_location_x = models.TextField(blank = True)
-        meet_location_y = models.TextField(blank = True)
-        message = models.TextField(blank = True)
-        count = models.IntegerField()
